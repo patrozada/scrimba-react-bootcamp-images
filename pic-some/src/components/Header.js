82a173046b52a8e71600c5fec927c5210fb6a011
AzureAD\PatriciaRozada <patricia.rozada@ultimasoftware.com>
2021-04-12 11:02:31 +0200

    Add Links to header

--- pic-some/src/components/Header.js
+++ pic-some/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 export default function Header() {
 	return (
 		<div>
 			Header :)
+			<Link to='/'>Back home</Link>
+			<Link to='/cart'>Cart</Link>
 		</div>
 	)
 }
